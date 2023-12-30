4a7dcb14eae845e5dc1e81dc3798e5ebd6b93bb7
mirron4k <86231350+mirron4k@users.noreply.github.com>
2023-12-30 22:14:10 +0300

    Add files via upload

--- com_help.lua
+++ com_help.lua
@@ -1,24 +1,3 @@
-script_name("moonloader-script-updater-example")
-script_version("25.06.2022")
-
-
-
--- https://github.com/mirron4k/scripts
-local enable_autoupdate = true -- false to disable auto-update + disable sending initial telemetry (server, moonloader version, script version, samp nickname, virtual volume serial number)
-local autoupdate_loaded = false
-local Update = nil
-if enable_autoupdate then
-    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..' .   c '..thisScript().version..'  '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format(' %d  %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('  .')sampAddChatMessage(b..' !',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'  .   ..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..':   .')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..':    .      '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout,     .      '..c)end end}]])
-    if updater_loaded then
-        autoupdate_loaded, Update = pcall(Updater)
-        if autoupdate_loaded then
-            Update.json_url = "https://raw.githubusercontent.com/mirron4k/scripts/main/version.json" .. tostring(os.clock())
-            Update.prefix = "[" .. string.upper(thisScript().name) .. "]: "
-            Update.url = "https://github.com/mirron4k/scripts"
-        end
-    end
-end
-
 function main()
 	if not isSampLoaded() or not isSampfuncsLoaded() then 
 		return 
@@ -27,9 +6,63 @@ function main()
 		wait(100) 
 	end
 
-	if autoupdate_loaded and enable_autoupdate and Update then
-        pcall(Update.check, Update.json_url, Update.prefix, Update.url)
-    end
+	autoupdate("http://qrlk.me/dev/moonloader/getgun/stats.php", '['..string.upper(thisScript().name)..']: ', "http://vk.com/qrlk.mods")
+
+	function autoupdate(json_url, prefix, url)
+		local dlstatus = require('moonloader').download_status
+		local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+		if doesFileExist(json) then os.remove(json) end
+		downloadUrlToFile(json_url, json,
+		  function(id, status, p1, p2)
+			if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+			  if doesFileExist(json) then
+				local f = io.open(json, 'r')
+				if f then
+				  local info = decodeJson(f:read('*a'))
+				  updatelink = info.updateurl
+				  updateversion = info.latest
+				  f:close()
+				  os.remove(json)
+				  if updateversion ~= thisScript().version then
+					lua_thread.create(function(prefix)
+					  local dlstatus = require('moonloader').download_status
+					  local color = -1
+					  sampAddChatMessage((prefix..'Ξανΰπσζενξ ξανξβλενθε. Οϋςΰώρό ξανξβθςόρÿ c '..thisScript().version..' νΰ '..updateversion), color)
+					  wait(250)
+					  downloadUrlToFile(updatelink, thisScript().path,
+						function(id3, status1, p13, p23)
+						  if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+							print(string.format('Ηΰγπσζενξ %d θη %d.', p13, p23))
+						  elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+							print('Ηΰγπσηκΰ ξανξβλενθÿ ηΰβεπψενΰ.')
+							sampAddChatMessage((prefix..'Ξανξβλενθε ηΰβεπψενξ!'), color)
+							goupdatestatus = true
+							lua_thread.create(function() wait(500) thisScript():reload() end)
+						  end
+						  if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+							if goupdatestatus == nil then
+							  sampAddChatMessage((prefix..'Ξανξβλενθε οπξψλξ νεσδΰχνξ. Ηΰοσρκΰώ σρςΰπεβψσώ βεπρθώ..'), color)
+							  update = false
+							end
+						  end
+						end
+					  )
+					  end, prefix
+					)
+				  else
+					update = false
+					print('v'..thisScript().version..': Ξανξβλενθε νε ςπεασεςρÿ.')
+				  end
+				end
+			  else
+				print('v'..thisScript().version..': Νε μξγσ οπξβεπθςό ξανξβλενθε. Ρμθπθςερό θλθ οπξβεπόςε ρΰμξρςξÿςελόνξ νΰ '..url)
+				update = false
+			  end
+			end
+		  end
+		)
+		while update ~= false do wait(100) end
+	  end
 
 	wait(2000)
 	sampRegisterChatCommand("dm", dm)
@@ -69,7 +102,7 @@ function main()
 	sampRegisterChatCommand("nrpaks", nrpaks)
 	sampRegisterChatCommand("chelp", chelp)
 	wait(500)
-	sampAddChatMessage("{f09205}[Aries-Help]: {FFFFFF}Σροεψνξ ηΰγπσζενξ!", -1)
+	sampAddChatMessage("{f09205}[Aries-Help]: {FFFFFF} !", -1)
 	sampAddChatMessage("{f09205}[Aries-Help]: {FFFFFF}   - {f09205}/chelp", -1)
 	sampAddChatMessage("{f09205}[Aries-Help]: {FFFFFF}   {f09205}Aries RolePlay", -1)
 	sampAddChatMessage("{f09205}[Aries-Help]: {FFFFFF}Version: {f09205}1.0.3", -1)
